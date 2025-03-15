3ee33d7635579313fe5871adf45f77a448f4bcf7
ArmanPefourque <armanpefo@gmail.com>
2025-03-14 19:48:42 -0700

    Intitialize some variables

--- engine.py
+++ engine.py
@@ -1,1 +1,13 @@
-# Backend physics engine coded in Python
+import math
+import random
+import time
+
+# International system of units (let's not recreate the time NASA got the metric system confused and caused the destruction of a multimillion dollar probe)
+# Also using real world data
+FUSELAGE_WEIGHT = 10000
+FUEL = 100000
+THRUST = 1500000 # Newtons
+USAGE_RATE = 500 # kg/s
+
+GRAVITY = 9.81
+
