b57b109fc27b23b80324a55879ee4c6bc8b39264
ArmanPefourque <armanpefo@gmail.com>
2025-03-14 19:54:48 -0700

    lol

--- engine.py
+++ engine.py
@@ -8,6 +8,20 @@ FUSELAGE_WEIGHT = 10000
 FUEL = 100000
 THRUST = 1500000 # Newtons
 USAGE_RATE = 500 # kg/s
+CROSS_SECTIONAL = 10  # m^2 (frontal area)
 
+AIR_DENSITY = 1.225
+DRAG_COEFICIENT = 0.3  # Drag coefficient (simplified)
+SCALE_HEIGHT = 8500 # exponential decrease as atmosphere gets thinner at higher altitudes
 GRAVITY = 9.81
+
+sim_time = 0
+sim_speed = 1 # x1 speed
+
+def ResetSimulation(speed):
+    global sim_time
+    global sim_speed
+
+    sim_time = 0
+    sim_speed = speed
 
